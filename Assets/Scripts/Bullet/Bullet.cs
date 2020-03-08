c6433227c15eb39466589464823751932f2bb681
Thomas <wapps3@gmail.com>
2020-03-08 15:21:37 +0100

    Add prefab Bullet

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public void Start()
+    {
+        Destroy(gameObject,1f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
