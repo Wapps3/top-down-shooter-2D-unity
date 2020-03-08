aa006a05fb581bbba98b12be77d62041db6330e6
Thomas <wapps3@gmail.com>
2020-03-08 15:22:17 +0100

    Player can shoot

---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public List<Transform> firePoint;
+    private int lastIndexShoot = 0;
+
+
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab,firePoint[lastIndexShoot].position, firePoint[lastIndexShoot].rotation);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint[lastIndexShoot].up * bulletForce, ForceMode2D.Impulse);
+
+        if (lastIndexShoot == firePoint.Count - 1)
+        {
+            lastIndexShoot = 0;
+        }
+        else
+        {
+            lastIndexShoot++;
+        }
+
+    }
+}
