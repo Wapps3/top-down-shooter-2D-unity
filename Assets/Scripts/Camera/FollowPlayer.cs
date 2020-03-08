6f6ec349b94a12f58c10f89b08b15d7dcc6d9f62
Thomas <wapps3@gmail.com>
2020-03-08 15:22:02 +0100

    Add movement control to player

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float speedLerp;
+    public Transform playerToFollow;
+    private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = playerToFollow.position - gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, playerToFollow.position - offset, Time.deltaTime * speedLerp);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    public Rigidbody2D rigidbody;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * speed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rigidbody.position;
+
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg  - 90f;
+
+        rigidbody.rotation = angle;
+    }
+}
