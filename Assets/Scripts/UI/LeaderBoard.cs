9da878619e6c5a65213fe1b7e2aeee80e7d5989d
Wapps <wapps3@gmail.com>
2021-01-29 09:28:30 +0100

    Add script to handle the end game canvas

---
+++ Assets/Scripts/UI/LeaderBoard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderBoard : MonoBehaviour
+{
+    public Button ReplayButton;
+    public Button ExitButton;
+
+    public List<Text> LeaderBoardScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReplayButton.onClick.AddListener(Replay);
+        ExitButton.onClick.AddListener(Exit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Replay()
+    {
+
+    }
+
+    void Exit()
+    {
+
+    }
+}
